beaaf9672c8b9081be0935077173b3069ca24433
Franco-Villanueva <franco.villanueva.emanuel@gmai.com>
2023-11-22 20:56:50 -0300

    commit modal

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the modal and buttons
-    var modal = document.getElementById("myModal");
-    var openBtn = document.getElementById("openModalBtn");
-    var closeBtn = document.getElementById("closeModalBtn");
+    // Get all modals and buttons
+    var modals = document.querySelectorAll(".modal");
+    var openBtns = document.querySelectorAll(".button"); // Cambiado para seleccionar todos los botones con la clase "button"
+    var closeBtns = document.querySelectorAll(".close");
 
-    // Open the modal
-    openBtn.onclick = function () {
-        modal.style.display = "block";
-    };
+    // Loop through each button to attach event listeners
+    openBtns.forEach(function (openBtn, index) {
+        // Attach click event to open the corresponding modal
+        openBtn.onclick = function () {
+            modals[index].style.display = "block";
+        };
+    });
 
-    // Close the modal
-    closeBtn.onclick = function () {
-        modal.style.display = "none";
-    };
+    // Loop through each button to attach event listeners
+    closeBtns.forEach(function (closeBtn, index) {
+        // Attach click event to close the corresponding modal
+        closeBtn.onclick = function () {
+            modals[index].style.display = "none";
+        };
+    });
 
     // Close the modal if clicked outside the content
     window.onclick = function (event) {
-        if (event.target === modal) {
-            modal.style.display = "none";
-        }
+        modals.forEach(function (modal, index) {
+            if (event.target === modal) {
+                modals[index].style.display = "none";
+            }
+        });
     };
 });
